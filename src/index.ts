d2d9fcc4624a6abc16c781be965e057012822dbf
Jorge Rangel <georgerangelcode@gmail.com>
2024-02-06 11:16:47 -0500

    Create WS server with Node.js, typescript, socket.io and express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("it works!!! ok")
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const port = 8080;
+const app = express();
+app.use(cors);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user is connected");
+
+  socket.on("disconnect", () => {
+    console.log("user is disconnect");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Lisening to server on ${port}`);
+});
